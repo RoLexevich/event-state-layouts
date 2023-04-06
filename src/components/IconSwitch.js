2ea222802c37bd6ad623aaca4dce0ddf194400bf
Родионов Алексей Игоревич <rodionov_a@tii.ru>
2023-04-06 20:31:09 +0300

    build commit

--- src/components/IconSwitch.js
+++
@@ -1,9 +0,0 @@
-export const IconSwitch = ({ icon, onSwitch }) => {
-  return (
-    <div>
-      <span className="material-icons" onClick={onSwitch}>
-        {icon}
-      </span>
-    </div>
-  );
-};
